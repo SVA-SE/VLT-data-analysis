73c6ccad83aa5ff191c1bc03e7a367d91304f0c2
Wiktor Gustafsson <wiktor.gustafsson@sva.se>
2019-08-01 16:04:47 +0200

    Finished data cleaning (hopefully)

--- wildlife_dashboard.R
+++ wildlife_dashboard.R
@@ -4,20 +4,39 @@ library(tidyverse)
 library(stringr)
 library(lubridate)
 
-# import all the wildlife data and fix/extract the dates
-# what the dates represent may differ between datasets 
-# wild_1986_2006 dates are autopsy dates, while wild_1992_2006 dates are arrival dates. 
+# manual edits have been done in csv copies of the original data files. These include:
+#   -removing unnecessary (and adding necessary) whitespace
+#   -repairing broken rows with missing columns
+#   -swapping class and order columns (they were named wrong in some tables)
+#   -fixing incorrect classification fields:
+#       *renaming swedish fields to latin
+#       *filling in some blanks
+#       *correcting spelling errors
+
+# import all the wildlife data, select relevant columns (rename for inter-dataset consistency), and fix/extract the dates
+# what the dates represent may differ between datasets
+# wild_1986_2006 dates are autopsy dates, while wild_1992_2006 dates are arrival dates.
+
 
 wild_1948_1981 <- read.csv2(
   "data/csv/1.1.1948-1981.csv",
   sep = ";",
   dec = ",",
   quote = "\"",
-  na.strings = c("", " ", ".", "Not given", "Not relevant", "Undefined", "?"),
+  na.strings = c("", " ", ".", "Not given", "Not relevant", "Undefined", "?", "-"),
   strip.white = TRUE
 ) %>%
+  select(ID = V.nr,
+         Animal = Djurslag,
+         Family,
+         Order,
+         Class,
+         Region = Län,
+         Place = Ort,
+         Date = Datum) %>%
   mutate(
-    Date = as.Date(as.character(Datum), format = "%Y%m%d"),
+    Region = as.factor(str_to_upper(Region)),
+    Date = as.Date(as.character(Date), format = "%Y%m%d"),
     Year = year(Date),
     Month = month(Date),
     Week = week(Date),
@@ -25,17 +44,28 @@ wild_1948_1981 <- read.csv2(
   )
 
 unique(wild_1948_1981$Class)
+unique(wild_1948_1981$Order)
+unique(wild_1948_1981$Family)
 
 wild_1982_1985 <- read.csv2(
   "data/csv/1.2.1982-1985.csv",
   sep = ";",
   dec = ",",
   quote = "\"",
-  na.strings = c("", " ", ".", "Not relevant", "Undefined", "?"),
+  na.strings = c("", " ", ".", "Not relevant", "Undefined", "?", "Not given", "-"),
   strip.white = TRUE
 ) %>%
+  select(ID = V.nr,
+         Animal = Djurslag,
+         Family,
+         Order,
+         Class,
+         Region = Län,
+         Place = Ort,
+         Date = Datum) %>%
   mutate(
-    Date = as.Date(as.character(Datum), format = "%Y%m%d"),
+    Region = as.factor(str_to_upper(Region)),
+    Date = as.Date(as.character(Date), format = "%Y%m%d"),
     Year = year(Date),
     Month = month(Date),
     Week = week(Date),
@@ -43,111 +73,48 @@ wild_1982_1985 <- read.csv2(
   )
 
 unique(wild_1982_1985$Class)
+unique(wild_1982_1985$Order)
+unique(wild_1982_1985$Family)
+
 
 wild_1986_2006 <- read.csv2(
   "data/csv/2.1986-2006.csv",
   sep = ";",
   dec = ",",
   quote = "\"",
-  na.strings = c("", " ", ".", "Not given", "Not relevant", "Undefined", "?"),
+  na.strings = c("", " ", ".", "Not given", "Not relevant", "Undefined", "?", "-"),
   strip.white = TRUE
 ) %>%
-  mutate(
-    Date = as.Date(ifelse(length(Obduktionsdatum.1) == 5, paste('0', as.character(Obduktionsdatum.1)), as.character(Obduktionsdatum.1)), format = "%y%m%d"),
-    Year = year(Date),
-    Month = month(Date),
-    Week = week(Date),
-    yearmonth = paste(Year, Month, sep = '-')
-  )
+  select(
+    ID = V.nr,
+    Animal = DJURSLAG,
+    Family,
+    Order,
+    Class,
+    Region = Län,
+    Place = Fyndort,
+    Gisx = X.koordinat,
+    Gisy = Y.koordinat,
+    Date = Obduktionsdatum.1
+  ) %>%
+mutate(
+  Region = as.factor(str_to_upper(Region)),
+  Gisx = as.numeric(Gisx),
+  Gisy = as.numeric(Gisy),
+  Date = as.Date(ifelse(
+    length(as.character(Date)) == 5,
+    paste('0', as.character(Date)),
+    as.character(Date)
+  ), format = "%y%m%d"),
+  Year = year(Date),
+  Month = month(Date),
+  Week = week(Date),
+  yearmonth = paste(Year, Month, sep = '-')
+)
 
 unique(wild_1986_2006$Class)
-
-wild_1992_2006 <- read.csv2(
-  "data/csv/3.1992-2006.csv",
-  sep = ";",
-  dec = ",",
-  quote = "\"",
-  na.strings = c("", " ", ".", "?", "Not relevant", "Not given"),
-  strip.white = TRUE
-) %>%
-  mutate(
-    Date = as.Date(as.character(ANKOMSTDATUM), format = "%Y-%m-%d"),
-    Year = year(Date),
-    Month = month(Date),
-    Week = week(Date),
-    yearmonth = paste(Year, Month, sep = '-')
-    # Class = replace(Class, Class == 0, NA)
-  ) 
-
-unique(wild_1992_2006$Class)
-
-wild_2007_2018 <- read.csv2(
-  "data/csv/4.2007-2018.csv",
-  sep = ";",
-  dec = ",",
-  quote = "\"",
-  na.strings = c("", " ", ".", "?", "#SAKNAS!", "Not given"),
-  strip.white = TRUE
-) %>%
-  mutate(
-    Date = as.Date(str_match(UppdragID, "^U(\\d{6})-")[seq.int(n()), 2], format = "%y%m%d"),
-    Year = year(Date),
-    Month = month(Date),
-    Week = week(Date),
-    yearmonth = paste(Year, Month, sep = '-')
-  )
-
-unique(wild_2007_2018$Class)
-
-# 2019-07-31 ended here
-
-unique(wild_1948_1981$Order)
-unique(wild_1982_1985$Order)
 unique(wild_1986_2006$Order)
-unique(wild_1992_2006$Order)
-unique(wild_2007_2018$Order)
-
-unique(wild_1948_1981$Family)
-unique(wild_1982_1985$Family)
 unique(wild_1986_2006$Family)
-unique(wild_1992_2006$Family)
-unique(wild_2007_2018$Family)
-
-## fixed some columns (e.g., "0" for order and class, 'class' and 'order' swapped) on the excel file copies in my H 
-## and also corrected for the different forms of NAs as below
-
-wild_1982_1985$Class[wild_1982_1985$Class=="Not relevant"] <- NA
-wild_1982_1985$Class[wild_1982_1985$Class=="Not given"] <- NA
-
-wild_1986_2006$Class[wild_1986_2006$Class=="Not given"] <- NA
-wild_1986_2006$Class[wild_1986_2006$Class=="Not relevant"] <- NA
-wild_1986_2006$Class[wild_1986_2006$Class=="Undefined"] <- NA
-
-wild_1992_2006$Class[wild_1992_2006$Class=="Not given"] <- NA
-wild_1992_2006$Class[wild_1992_2006$Class=="Not relevant"] <- NA
-
-wild_2007_2018$Class[wild_2007_2018$Class=="Not given"] <- NA
-
-#make a dataset for animal species, time and geography
-##1992_2006 dataset doesn't have a län column and for 'ort' information there is 'postadress' and 'inpostadress', used 'inpostadress' for now but needs checking.
-
-variable1 <- c("V-nr", "Djurslag", "Family","Order", "Class", "L?n","Ort", "Date", "Year", "Month", "Week", "yearmonth" )
-W1_species_time_geo <- wild_1948_1981[variable1]
-W2_species_time_geo <- wild_1982_1985[variable1]
-colnames(W1_species_time_geo)[1] <- "ID"
-colnames(W2_species_time_geo)[1] <- "ID"
-
-variable2 <- c("V-nr", "DJURSLAG", "Family","Order", "Class", "L?n__1", "Fyndort", "Y-koordinat", "X-koordinat", "Date", "Year", "Month", "Week", "yearmonth" )
-W3_species_time_geo <- wild_1986_2006[variable2]
-colnames(W3_species_time_geo)[1] <- "ID"
-colnames(W3_species_time_geo)[2] <- "Djurslag"
-colnames(W3_species_time_geo)[6] <- "Län"
-colnames(W3_species_time_geo)[7] <- "Ort"
-colnames(W3_species_time_geo)[8] <- "Gisy"
-colnames(W3_species_time_geo)[9] <- "Gisx"
-
-W3_species_time_geo$Gisx <- as.numeric(W3_species_time_geo$Gisx)
-W3_species_time_geo$Gisy <- as.numeric(W3_species_time_geo$Gisy)
 
 variable3 <- c("UPPDRAGSID", "DJURSLAG", "Family", "Order", "Class", "INSPOSTADRESS", "X_KOORDINAT", "Y_KOORDINAT", "Date", "Year", "Month", "Week", "yearmonth")
 W4_species_time_geo <- wild_1992_2006[variable3]
@@ -157,13 +124,82 @@ colnames(W4_species_time_geo)[6] <- "Ort"
 colnames(W4_species_time_geo)[7] <- "Gisy"
 colnames(W4_species_time_geo)[8] <- "Gisx"
 
+wild_1992_2006 <- read.csv2(
+  "data/csv/3.1992-2006.csv",
+  sep = ";",
+  dec = ",",
+  quote = "\"",
+  na.strings = c("", " ", ".", "?", "Not relevant", "Not given", "Undefined", "-"),
+  strip.white = TRUE
+) %>%
+  select(
+    ID = UPPDRAGSID,
+    Animal = DJURSLAG,
+    Family,
+    Order,
+    Class,
+    Place = INSPOSTADRESS,
+    Gisx = X_KOORDINAT,
+    Gisy = Y_KOORDINAT,
+    Date = ANKOMSTDATUM
+  ) %>%
+  mutate(
+    Gisx = as.numeric(Gisx),
+    Gisy = as.numeric(Gisy),
+    Date = as.Date(as.character(Date), format = "%Y-%m-%d"),
+    Year = year(Date),
+    Month = month(Date),
+    Week = week(Date),
+    yearmonth = paste(Year, Month, sep = '-')
+  ) 
+
+unique(wild_1992_2006$Class)
+unique(wild_1992_2006$Order)
+unique(wild_1992_2006$Family)
+
+wild_2007_2018 <- read.csv2(
+  "data/csv/4.2007-2018.csv",
+  sep = ";",
+  dec = ",",
+  quote = "\"",
+  na.strings = c("", " ", ".", "?", "#SAKNAS!", "Not given", "Undefined", "-"),
+  strip.white = TRUE
+) %>%
+  select(
+    ID = UppdragID,
+    Animal = Djurslag,
+    Family,
+    Order,
+    Class,
+    Region = Län,
+    Place = Postort,
+    Gisx,
+    Gisy,
+  ) %>%
+  mutate(
+    Region = as.factor(str_to_upper(Region)),
+    Gisx = as.numeric(Gisx),
+    Gisy = as.numeric(Gisy),
+    Date = as.Date(str_match(ID, "^U(\\d{6})-")[seq.int(n()), 2], format = "%y%m%d"),
+    Year = year(Date),
+    Month = month(Date),
+    Week = week(Date),
+    yearmonth = paste(Year, Month, sep = '-')
+  )
+
+unique(wild_2007_2018$Class)
+unique(wild_2007_2018$Order)
+unique(wild_2007_2018$Family)
+
+# 2019-08-01 ends here
+
+#make a dataset for animal species, time and geography
+##1992_2006 dataset doesn't have a län column and for 'ort' information there is 'postadress' and 'inpostadress', used 'inpostadress' for now but needs checking.
+
 #W4_species_time_geo$Län <- NA
 #W4_species_time_geo <- W4_species_time_geo[c("ID", "Djurslag", "Family","Order", "Class", "Län","Ort", "Gisx", "Gisy", "Date", "Year", "Month", "Week", "yearmonth")]
 
-variable4 <- c("UppdragID", "Djurslag", "Family","Order", "Class", "L?n","Postort", "Gisx", "Gisy", "Date", "Year", "Month", "Week", "yearmonth" )
-W5_species_time_geo <- wild_2007_2018[variable4]
-colnames(W5_species_time_geo)[1] <- "ID"
-colnames(W5_species_time_geo)[7] <- "Ort"
+
 
 #merge together and make into one dataset
 
